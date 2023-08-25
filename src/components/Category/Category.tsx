7f9492c0325c8c6969da0741076f593ca80e6109
SamJHudson01 <samjhudson@gmail.com>
2023-08-25 11:19:11 +0100

    feat: create basic category list item

---
+++ src/components/Category/Category.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Category.css";
+
+const Category = () => {
+  return (
+    <div className="category">
+      <p className="category-name">Test Category</p>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/components/CategoryList/CategoryList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Category from "../Category/Category";
+
+const CategoryList = () => {
+  return (
+    <div>
+      <h2>Category List</h2>
+      <Category />
+    </div>
+  );
+};
+
+export default CategoryList;
