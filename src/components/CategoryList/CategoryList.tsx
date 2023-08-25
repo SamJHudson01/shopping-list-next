82ba229dadf3e054eb2aee70beda94877ee12ef1
SamJHudson01 <samjhudson@gmail.com>
2023-08-25 11:40:33 +0100

    add basic styling to category list. add fixed categories to list

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { FixedCategories } from "../../utils/fixedCategories";
 import Category from "../Category/Category";
+import "./CategoryList.css";
 
 const CategoryList = () => {
   return (
-    <div>
-      <h2>Category List</h2>
-      <Category />
+    <div className="category-list">
+      <h4>Category List</h4>
+      {FixedCategories.map((category) => (
+        <Category
+          key={category.id}
+          name={category.name}
+          color={category.color}
+          id={category.id}
+        />
+      ))}
     </div>
   );
 };
