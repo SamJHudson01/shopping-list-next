932fd645e236d9f1607319fd98b68920198d1877
SamJHudson01 <samjhudson@gmail.com>
2023-08-25 13:37:29 +0100

    test:add test coverage report to config

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,27 @@
 module.exports = {
-    roots: ['<rootDir>'],
-    globals: {
-        'ts-jest': {
-          tsconfig: 'tsconfig.jest.json'
-        }
-      },
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/$1',
-    },
-   // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // setupFiles before v9
-  };
+  roots: ['<rootDir>'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json'
+    }
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx,js,jsx}',
+    '!<rootDir>/src/**/*.interface.ts',
+    '!<rootDir>/src/**/*.mock.ts',
+    '!<rootDir>/src/**/*.module.ts',
+    '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/**/*.test.ts',
+    '!<rootDir>/src/**/*.d.ts'
+  ],
+  collectCoverage: true,
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // setupFiles before v9
+};
