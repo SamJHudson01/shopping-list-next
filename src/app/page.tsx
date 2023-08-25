ef69f34045104d08590ab30194eebcb5cafee0f9
SamJHudson01 <samjhudson@gmail.com>
2023-08-25 11:19:11 +0100

    feat: create basic category list item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import ShoppingList from '../components/ShoppingList/ShoppingList'
-
+import Image from "next/image";
+import styles from "./page.module.css";
+import ShoppingList from "../components/ShoppingList/ShoppingList";
+import CategoryList from "../components/CategoryList/CategoryList";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <ShoppingList />
+      <CategoryList />
     </main>
-  )
+  );
 }
